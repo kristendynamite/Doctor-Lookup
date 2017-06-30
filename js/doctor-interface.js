8ae1d95fc68b96b13de23fbfbb04c7a554383bb3
Kristen <kristen.dynamite@gmail.com>
2017-06-30 12:08:20 -0700

    add back-end logic in order to make doctor bio appear in console

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,5 +1,3 @@
-//receive form input
-
 var Doctor = require('./../js/doctor.js').doctorModule;
 var apiKey = require('./../.env').apiKey;
 
@@ -11,17 +9,20 @@ var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   var currentDoctorObject = new Doctor();
-  currentDoctorObject.getDoctors();
+  //receive form input
   $('#doctor-form').submit(function(event) {
     event.preventDefault();
      var goal = $('#goal').val();
+     var output = Doctor(goal);
+     currentDoctorObject.getDoctors(goal);
     $('#results').text("The medical issue you've specified is " + goal + ". Here are a list of doctors near you who can treat this medical issue:");
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ goal + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-    .then(function(result) {
-      console.log(result);
-    })
-   .fail(function(error){
-      console.log("fail");
-    });
+    $('#results').append("<li>" + output + "</li>")
+    // exports.getDoctors = function(medicalIssue) {
+
+      // };
+    // output.forEach(function(element) {
+    //   $('#results').append("<li>" + element + "</li>");
+    // });
+
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,22 +1,24 @@
+var apiKey = require('./../.env').apiKey;
+
 //make an API request and display results
+// constructor
 
-// exports.getDoctors = function(medicalIssue) {
-//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-//    .then(function(result) {
-//       console.log(result);
-//     })
-//    .fail(function(error){
-//       console.log("fail");
-//     });
-// };
-
-function Doctor() {
-  // constructor
+function Doctor(name) {
+  this.name = name;
 }
 
-Doctor.prototype.getDoctors = function() {
-  console.log("Hi! I'm a doctor object.");
+Doctor.prototype.getDoctors = function(goal) {
+  var output = [];
   // method code
-};
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ goal+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+      var output = result.data[0].profile.bio;
+      console.log(output);
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
+    return output;
+  };
 
 exports.doctorModule = Doctor;
