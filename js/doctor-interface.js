9c642a83b5a2a59540ba79c774b95288f5d440fe
Kristen <kristen.dynamite@gmail.com>
2017-06-30 09:21:46 -0700

    add function that allows user to submit form and see message confirming their medical issue

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,8 +1,28 @@
 //receive form input
 
 var Whatever = require('./../js/doctor.js').calculatorModule;
+var apiKey = require('./../.env').apiKey;
+
+// $(document).ready(function() {
+//   var simpleCalculator = new Whatever("a string");
+//   var output = simpleCalculator.pingPong(1000);
+// });
+
+// $(document).ready(function() {
+//   $('.doctor-form').submit(function(event) {
+//     event.preventDefault();
+//     var goal = $('#goal').val();
+//     var output = doctorLookup(goal);
+//     output.forEach(function(element) {
+//       $('#results').append("<li>" + element + "</li>");
+//       });
+//     });
+//   });
 
 $(document).ready(function() {
-  var simpleCalculator = new Whatever("a string");
-  var output = simpleCalculator.pingPong(1000);
+  $('#doctor-form').submit(function(event) {
+    event.preventDefault();
+     var goal = $('#goal').val();
+    $('#results').text("The medical issue you've specified is " + goal + ". Here are a list of doctors near you who can treat this medical issue:");
+  });
 });
